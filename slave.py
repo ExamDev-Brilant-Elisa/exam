8d0e41a302b0200835afb27354312972d6eaab6b
kotzyr <57570454+kotzyr@users.noreply.github.com>
2019-11-10 13:22:35 +0100

    Update slave.py

--- slave.py
+++ slave.py
@@ -1,13 +1,24 @@
 import socket as modsocket
+import datetime
 
-def connexion():
-    s = modsocket.socket(modsocket.AF_INET, modsocket.SOCK_STREAM)
+def ddos():
 
-    s.connect(("localhost", 5000))
+    if (datetime.date == 2019, 11, 7) and (datetime.time == 11, 59):
+        print("potentielle attaque ddos".encode("utf-8"))
+    else:
+        print("error".encode("utf-8"))
 
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-    s.close()
-    print ("Received", repr(data))
+s = modsocket.socket(modsocket.AF_INET, modsocket.SOCK_STREAM)
+server_addr = ("192.168.56.1", 60000)
+slave = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-connexion()
+slave.connect(server_addr)
+
+attaque = (slave.recv(1024))
+if attaque == "attc":
+    ddos()
+    slave.send("le slave a lancé le ddos".encode("utf-8"))
+else:
+    slave.send("euh problème".encode("utf-8"))
+
+slave.close()
