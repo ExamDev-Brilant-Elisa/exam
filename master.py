0268474d2dc8ef617f8f1d56ed12efcb1573eb91
kotzyr <57570454+kotzyr@users.noreply.github.com>
2019-11-10 14:14:47 +0100

    Update master.py

--- master.py
+++ master.py
@@ -1,19 +1,27 @@
 #master, programme du hackeur, que j'ai assimilé comme étant un serveur. il sait recevoir et envoyer des messages au slave
-import socket
+import socket as modsocket
 
-master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-master.bind(("192.168.56.1", 60000))
+server_addr = ("192.168.56.1", 60000)
+
+
+master = modsocket.socket(modsocket.AF_INET, modsocket.SOCK_STREAM)
+master.bind((server_addr))
+print("le maître est démarré...")
 
 master.listen()
-slave, addr = master.accept()
+print("en écoute...")
 
+slave, addr = master.accept()
 print("Connection accepted for : ", addr)
 
-slave.send("attc".encode("utf-8"))
-
-print(slave.recv(1024).decode("utf-8"))
+slave.send(b"attc")
+print(slave.recv(1024))
 
 master.close()
+
+
+
+
 
 
 
