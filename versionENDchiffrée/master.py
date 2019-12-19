0a9f799089df2b54767318afa278eb3da8b62b5a
kotzyr <57570454+kotzyr@users.noreply.github.com>
2019-12-19 09:57:29 +0100

    Update master.py

--- versionENDchiffrée/master.py
+++ versionENDchiffrée/master.py
@@ -12,6 +12,22 @@ from Crypto.Cipher import AES
 lock = threading.Lock()
 crypto = AES.new('This is a key437', AES.MODE_CBC, 'This is an IV916')
 
+# on initie les différents champs présents dans l'objet Connexion
+fichier = open("ListeIP.txt", "a")
+liste_ip = []
+chemin = ""
+ip = ""
+date = ""
+clients = []
+port = ""
+nb_line = ""
+adresse_machine = ("localhost", 5000)
+# carte reseau qui va nous servire pour la première méthode de l'objet Connexion
+carte_reseau = modSocket.socket(modSocket.AF_INET, modSocket.SOCK_STREAM)
+# on assigne l'objet choixAction qui est l'enfant de l'objet Connexion à la variable objetChoixAction
+objet_choix_action = Connexion(adresse_machine, carte_reseau, liste_ip, port, fichier, chemin, ip, date, clients,
+                               nb_line)
+
 """
 creation de l'objet Connexion, composé de 9 champs et 2 méthodes
 
@@ -164,22 +180,6 @@ class Connexion():
         except ConnectionResetError:
             print("un slave c'est déconnecté")
 
-
-# on initie les différents champs présents dans l'objet Connexion
-fichier = open("ListeIP.txt", "a")
-liste_ip = []
-chemin = ""
-ip = ""
-date = ""
-clients = []
-port = ""
-nb_line = ""
-adresse_machine = ("localhost", 5000)
-# carte reseau qui va nous servire pour la première méthode de l'objet Connexion
-carte_reseau = modSocket.socket(modSocket.AF_INET, modSocket.SOCK_STREAM)
-# on assigne l'objet choixAction qui est l'enfant de l'objet Connexion à la variable objetChoixAction
-objet_choix_action = Connexion(adresse_machine, carte_reseau, liste_ip, port, fichier, chemin, ip, date, clients,
-                               nb_line)
 
 # initiation d'argparse avec les arguments
 parser = argparse.ArgumentParser()
